c9f7119d8a4c27fcb8011ed218e363b433aadd55
Kim Nguyen <nnhk23@gmail.com>
2020-12-22 17:22:11 -0800

    added navs and containers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import Home from './component/Home'
+import TopNav from './component/TopNav'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  // fetch user
+  state={
+    user: {
+      id: 1, 
+      username: "alex", 
+      name: "Alex G", 
+      buys_left: 1
+    }
+  }
+
+  render(){
+    return (
+      <div>
+
+        <TopNav /> 
+          
+        <Container style={{marginLeft: 0}}>
+          <Home user={this.state.user} />
+        </Container>
+        
+        {/* <UserInfo user={this.state.user} /> */}       
+        {/* SignIn component */}
+      </div>
+      // <Router>
+      //   <div className="App">
+      //     <Route exact path="/" render={() => <Home />} />
+      //   </div>
+      // </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/SideNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SideNav = () => {
+    // appear after logged in
+    // render Alex's pets from Home
+    // buy pet buttons would trigger a callback function onClick.
+    // click on specific pet would trigger a callback function onClick.
+    // => re-render tamagotchi's screen
+    return(
+        <h2>Me SideNav</h2>
+    )
+}
+export default SideNav
