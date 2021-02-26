be0668f7448984b334a41bfc47c4fcf472c73e13
gamil91 <gamil.alexa@gmail.com>
2021-02-25 21:16:35 -0800

    debugged

--- src/components/RenderPetBtn.js
+++ src/components/RenderPetBtn.js
@@ -3,12 +3,12 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import '../css/RenderPetBtn.css'
 
-const RenderPetBtn = ({ userPet, handleUserPetIconClick }) => {
+const RenderPetBtn = ({ userPet, handleUserPetIconClick, isDisabled }) => {
     const { name, pet } = userPet
     const { species, img_icon } = pet
     
     return (
-        <Row className="icon-row" onClick={() => handleUserPetIconClick(userPet)}>
+        <Row className="icon-row" onClick={isDisabled ? null : () => handleUserPetIconClick(userPet)}>
             <Col sm={6}>
                 <img className="icon-img" src={img_icon} alt="pet img"></img>
             </Col>
